2faeed697e0b616b7b17b8f35eabecd5d12eaf28
jforgue <juforgue@gmail.com>
2019-12-04 15:47:09 -0500

    quick update

--- sketch_n0va/sketch_n0va.ino
+++ sketch_n0va/sketch_n0va.ino
@@ -4,9 +4,9 @@ unsigned long time;
 
 //Define microphone input signal pins
 #define micOut1 53
-#define micOut2 49
-#define micOut3 47
-#define micOut4 45
+#define micOut2 51
+#define micOut3 49
+#define micOut4 47
 
 // Define output pin
 #define output 31
@@ -27,7 +27,7 @@ void setup() {
   
   // Enable analog in
   pinMode(micOut1, INPUT_PULLUP);
-  //pinMode(micOut2, INPUT_PULLUP);
+  pinMode(micOut2, INPUT_PULLUP);
   //pinMode(micOut3, INPUT_PULLUP);
   //pinMode(micOut4, INPUT_PULLUP);
 }
@@ -41,7 +41,15 @@ class Mic {
  
 void loop() {
   //Start time in micros
-  time = micros();
+  time = millis();
+  Mic mic1;
+  mic1.name = "mic1";
+  Mic mic2;
+  mic2.name = "mic2";
+  Mic mic3;
+  mic3.name = "mic3";
+  Mic mic4;
+  mic4.name = "mic4";
   
   //Read analog
   bool in1 = digitalRead(micOut1);
@@ -49,18 +57,32 @@ void loop() {
   bool in3 = digitalRead(micOut3);
   bool in4 = digitalRead(micOut4);
 
-  boolean on = !(in1 || in2 || in3 || in4);
+  mic1.on = !in1;
+  mic2.on = !in2;
+
+  //Serial.print(mic1.on);
+  //Serial.println(mic2.on);
+
+  boolean on = !(in1 & in2);// && in3 && in4);
   if(on) {
     
-    Mic mic1;
-    mic1.name = "mic1";
-    mic1.on = in1;
     mic1.start_time = time;
+    mic2.start_time = time;
+      
 
-    Serial.print(mic1.name);
-    Serial.print(" time:");
-    Serial.println(mic1.start_time);
-    delay(100);
+    if (mic1.on) {
+      Serial.print(mic1.name);
+      Serial.print(" time=");
+      Serial.println(mic1.start_time);
+      delay(10);
+    } 
+    if (mic2.on) {
+      Serial.print(mic2.name);
+      Serial.print(" time=");
+      Serial.println(mic2.start_time);
+      delay(100);
+    }
+      
     //Start calculatttion
   }
 }
