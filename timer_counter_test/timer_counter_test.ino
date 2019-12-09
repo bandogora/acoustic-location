66fe14497213b249fda0856d37defb77ca4f4376
jforgue <juforgue@gmail.com>
2019-12-09 17:29:40 -0500

    working timer test

--- timer_counter_test/timer_counter_test.ino
+++ timer_counter_test/timer_counter_test.ino
@@ -1,58 +1,61 @@
 // Define microphone input signal pins
-#define micOut1 47  //ICP1
-#define micOut2 9   //ICP3
-#define micOut3 35  //ICP4
-#define micOut4 36  //ICP5
+#define ICPpin1 14  // pin 47 ICP1
+#define ICPpin3 21  // pin 9 ICP3
+#define ICPpin4 49  // pin 35 ICP4
+#define ICPpin5 48  // pin 36 ICP5
 
 // Define output pin
-#define output 31
+//#define output 31
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(19200);
   
-  // Pull up digital in pins
-  pinMode(micOut1, INPUT_PULLUP);
-  pinMode(micOut2, INPUT_PULLUP);
-  pinMode(micOut3, INPUT_PULLUP);
-  pinMode(micOut4, INPUT_PULLUP);
+  // Pull up digital input pins
+  pinMode(ICPpin1, INPUT_PULLUP);
+  pinMode(ICPpin3, INPUT_PULLUP);
+  pinMode(ICPpin4, INPUT_PULLUP);
+  pinMode(ICPpin5, INPUT_PULLUP);
 
-//  for (int n = 1; n < 6; n++) {
-//    if ( n != 2) {
-//      TIFR[n] = 1<<ICF[n];    // Clear ICF1. Clear pending interrupts
-//      TCCR[n]B = 1<<CS[n]0;   // Timer clock on, no pre-scalling
-//      TIMSK[n] = 1<<ICIE[n];  // Enable 16-bit Timer Capture Event Interrupt
-//      // |(0<<ICES[n]);       // set edge detection 0=falling 1=rising
-//    }
-//  }
-
-  TIFR1 = 1<<ICF1;
-  TCCR1B = 1<<CS10;
-  TIMSK1 = 1<<ICIE1;
+  // Timer 1 setup
+ 
+  TIFR1 = 1<<ICF1;              // Clear pending interupts
+  TCCR1B = (1<<CS11)|(1<<CS10); // Initiate timer with /64 prescale
+  TIMSK1 = 1<<ICIE1;            // Enable 16-bit Timer Capture Event Interrupt
+  TCNT1 = 0;                    // Reset timer
+  
+  // Timer 3 setup
   TIFR3 = 1<<ICF3;
-  TCCR3B = 1<<CS30;
-  TIMSK3 = 1<<ICIE3;
+  TCCR3B = (1<<CS31)|(1<<CS30);
+  TIMSK1 = 1<<ICIE3;
+  TCNT3 = 0;
+  
+  // Timer 4 setup
+  TIFR4 = 1<<ICF4;
+  TCCR4B = (1<<CS41)|(1<<CS40);
+  TIMSK4 = 1<<ICIE4;
+  TCNT4 = 0;
+  
+  // Timer 5 setup
+  TIFR5 = 1<<ICF5;
+  TCCR5B = (1<<CS51)|(1<<CS50);
+  TIMSK5 = 1<<ICIE5;
+  TCNT5 = 0;
 }
  
 void loop() {
 
   
   // Read Input Capture Register
-  bool in1 = digitalRead(micOut1);
+  bool in1 = digitalRead(ICPpin1);
+  bool in2 = digitalRead(ICPpin3);
+  bool in3 = digitalRead(ICPpin4);
+  bool in4 = digitalRead(ICPpin5);
   
-  word timer_val1 = ICR1;
-  if (!in1) {
-    Serial.println("On");
-    Serial.println(timer_val1);
-    Serial.println(TCNT1);
-    Serial.println(ICF1);    
+  bool on = !(in1 | in2);
+  if (!in4) {
+    Serial.println(ICR5); // Time of interrupt
+    //Serial.println(ICR3); // Time of interrupt
     delay(100);  
   }
-  //TCNT1 = 0;
-//  bool in2 = digitalRead(micOut2);
-//  word timer_val2 = ICR3;
-//  //Serial.print(timer_val2);
-//  bool in3 = digitalRead(micOut3);
-//  word timer_val3 = ICR4;
-//  bool in4 = digitalRead(micOut4);
-//  word timer_val4 = ICR5;
+
 }
