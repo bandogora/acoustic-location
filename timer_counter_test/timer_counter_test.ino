c4f831343334b91af43486c82b7c4378ba768c02
jforgue <juforgue@gmail.com>
2019-12-11 00:34:15 -0500

    merge algorithom

--- timer_counter_test/timer_counter_test.ino
+++ timer_counter_test/timer_counter_test.ino
@@ -13,6 +13,9 @@
 #define ICPpin4 49                    // ICP4 - ATmega2560 pin 35
 #define ICPpin5 48                    // ICP5 - ATmega2560 pin 36
 
+/* Define lenght */
+unsigned int distance = 0.4;
+
 /* Define output pins */
 //#define output 31
 
@@ -29,32 +32,28 @@ void setup() {
   pinMode(ICPpin5, INPUT_PULLUP);
 
   /* Timer 1 setup */
-  TIFR1 = (1<<ICF1)|(1<<TOV1);         // Clear pending interupts and Overflow Flag Interrupt
-  TCCR1A = 0;                          // Disable compare interrupts (necessary for arduino) and set WGM to normal mode
-  TCCR1B = (1<<CS11)|(1<<CS10);        // Initiate timer with /64 prescale
-  TIMSK1 = (1<<ICIE1)|(1<<TOIE1);      // Enable 16-bit Timer Capture Event Interrupt and Overflow Interrupt
-  TCNT1 = 0;                           // Reset timer
+  TIFR1 = (1<<ICF1);                  // Clear pending interupts and Overflow Flag Interrupt
+  TCCR1A = 0;                         // Disable compare interrupts (necessary for arduino) and set WGM to normal mode
+  TCCR1B = (1<<CS11)|(1<<CS10);       // Initiate timer with /64 prescale
+  TIMSK1 = (1<<ICIE1);                // Enable 16-bit Timer Capture Event Interrupt and Overflow Interrupt
   
   /* Timer 3 setup */
-  TIFR3 = (1<<ICF3)|(1<<TOV3);
+  TIFR3 = (1<<ICF3);
   TCCR3A = 0;
   TCCR3B = (1<<CS31)|(1<<CS30);
-  TIMSK1 = (1<<ICIE3)|(1<<TOIE3);
-  TCNT3 = 0;
+  TIMSK1 = (1<<ICIE3);
   
   /* Timer 4 setup */
-  TIFR4 = (1<<ICF4)|(1<<TOV4);
+  TIFR4 = (1<<ICF4);
   TCCR4A = 0;
   TCCR4B = (1<<CS41)|(1<<CS40);
-  TIMSK4 = (1<<ICIE4)|(1<<TOIE4);
-  TCNT4 = 0;
+  TIMSK4 = (1<<ICIE4);
   
   /* Timer 5 setup */
-  TIFR5 = (1<<ICF5)|(1<<TOV5);
+  TIFR5 = (1<<ICF5);
   TCCR5A = 0;
   TCCR5B = (1<<CS51)|(1<<CS50);
-  TIMSK5 = (1<<ICIE5)|(1<<TOIE5);
-  TCNT5 = 0;
+  TIMSK5 = (1<<ICIE5);
 }
  
 void loop() {
@@ -66,73 +65,162 @@ void loop() {
   bool on = !(in1 & in2 & in3 & in4); // Invert for falling edge
   
   if (on) {
-    char first =0;
-    if (in1 == 0) {
-      first = '0';
-    }
-    if (in2 == 0) {
-      first = '1';
-    }
-    if (in3 == 0) {
-      first = '2';
-    }
-    if (in4 == 0) {
-      first = '3';
-    }
-    Serial.print(first);
+    /* Reset timers */
+    TCNT1 = 0;
+    TCNT3 = 0;
+    TCNT4 = 0;
+    TCNT5 = 0;
+    
     /* Read ICR registers */
     unsigned char sreg = SREG;        // Save global interrupt flag
     noInterrupts();                   // Disable arduino interrupts
-
     unsigned int icr[4];              // Read ICRs
     icr[0] = ICR1;
     icr[1] = ICR3;
     icr[2] = ICR4;
     icr[3] = ICR5;
-    unsigned char tov[4];             // Read TOV flags
-    tov[0] = TOV1;
-    tov[1] = TOV3;
-    tov[2] = TOV4;
-    tov[3] = TOV5;
-    
     SREG = sreg;                      // Restore global interrupt flag
     interrupts();                     // Enable arduino interrupts
 
-    
-    //timeAdjust(icr);
+    /* Convert timer tics to seconds */
+    for (int i = 0; i < 4; i++) {
+      icr[i] *= 0.000004;
+    }
 
-
+    double* pos = getPosFromTimes(icr);
+    Serial.print("x: ");
+    Serial.println(pos[0]);
+    Serial.print("y: ");
+    Serial.println(pos[1]);
     
+    /* Determine which pin changed state first */
+    
+    /* Test
     Serial.print("Time 1: ");
     Serial.println(icr[0]);
-    Serial.println(tov[0]);
     Serial.print("Time 2: ");
     Serial.println(icr[1]);
     Serial.print("Time 3: ");
     Serial.println(icr[2]);
     Serial.print("Time 4: ");
     Serial.println(icr[3]);
+    */
   }
 }
 
-unsigned int timeAdjust(char first, unsigned int icr[]) {
-  unsigned int times[4];
-  switch (first) {
-    case '1':
-      times[0] = 0;
-      break;
-    case '2':
-      times[1] = 0;
-      break;
-    case '3':
-      times[2] = 0;
-      break;
-    case '4':
-      times[3] = 0;
-      break;
+double* getPosFromTimes(unsigned int Xt[4]) {
+  // Xt is an array that stores the TOA for mics from 0 to 3
+  // Distance is the length of the side of the square board in meter
+
+  //S data structure could be stored as global 
+  double S[4][2] = { {0,0},{1,0},{1,1},{0,1} };
+
+
+  for(int i = 0; i < 4; i++) {
+    for (int j = 0; j < 2; j++) {
+      S[i][j] *= distance;
+    }
   }
+
+
+  double c = 340;
   
-  for (int i = 1; i < 5; i++) {
-    
+  int min_index = minimum_index(Xt);
+
+  int x_next = 0;
+  int x_last = 0;
+
+  //min_index = 1;
+
+  switch (min_index) {
+    case 0:
+      x_next = 1;
+      x_last = 3;
+      break;
+    case 1:
+      x_next = 2;
+      x_last = 0;
+      break;
+    case 2:
+      x_next = 3;
+      x_last = 1;
+      break;
+    case 3:
+      x_next = 0;
+      x_last = 2;
+      break;
   }
+
+  double Xb;
+  double Xc;
+
+  Xb = c * (Xt[x_next] - Xt[min_index]);
+  Xc = c * (Xt[x_last] - Xt[min_index]);
+
+  double A;
+  double B;
+  double C;
+  double delta;
+
+  double r1;
+  double r2;
+
+  double x_res;
+  double y_res;
+
+  
+  A = pow(Xb, 2) + pow(Xc, 2) - 1;
+
+  B = -(  Xb *( 1 - pow(Xb, 2))   +   Xc*(1 - pow(Xc, 2))  );
+
+  C = pow((0.5 - 0.5 * pow(Xb, 2)), 2) + pow((0.5 - 0.5 * pow(Xc, 2)), 2);
+
+  //delta = sqrt(B ^ 2 - 4 * A * C);
+  delta = sqrt(pow(B, 2) - 4 * A * C);
+
+  r1 = (-B + delta) / (2 * A);
+  r2 = (-B - delta) / (2 * A);
+
+  x_res = 0.5 - 0.5 * pow(Xb, 2) - Xb * r2;
+  y_res = 0.5 - 0.5 * pow(Xc, 2) - Xc * r2;
+
+
+  switch (min_index) {
+  case 0:
+    x_res = y_res;
+    y_res = x_res;
+    break;
+  case 1:
+    x_res = distance - y_res;
+    y_res = x_res;
+    break;
+  case 2:
+    x_res = distance - x_res;
+    y_res = distance - y_res;
+    break;
+  case 3:
+    x_res = y_res;
+    y_res = distance - x_res;
+    break;
+  }
+
+  double pos[2];
+  pos[0] = x_res;
+  pos[1] = y_res;
+
+  return pos;
+
+}
+
+int minimum_index(unsigned int in[4]) {
+  int mini_index = 0;
+  double minimum = in[0];
+  for (int i = 0; i < 4;i++) {
+    if (minimum > in[i]) {
+      mini_index = i;
+      minimum = in[i];
+    }
+
+  }
+  return mini_index;
 }
