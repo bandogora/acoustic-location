8bbd7c030f5606a3928ead10baf0b6bd070fc4c0
jforgue <juforgue@gmail.com>
2020-02-18 17:08:26 -0500

    Fix TIMSK3 and minor refactoring

--- acoustic_location_sketch/acoustic_location_sketch.ino
+++ acoustic_location_sketch/acoustic_location_sketch.ino
@@ -1,15 +1,16 @@
 #include <avr/interrupt.h>
 #include <util/atomic.h>
+#define F_CPU 16000000UL  // 16 MHz
+#include <util/delay.h>
 
 /* Define microphone input signal pins */
-#define ICPpin1 43  // ICP1 - ATmega2560 pin 47
-#define ICPpin3 42  // ICP3 - ATmega2569 pin 9
-#define ICPpin4 49  // ICP4 - ATmega2560 pin 35
-#define ICPpin5 48  // ICP5 - ATmega2560 pin 36
+//ICP1 - Arduino pin 43 - ATmega2560 pin 47
+//ICP3 - Arduino pin 42 - ATmega2569 pin 9
+//ICP4 - Arduino pin 49 - ATmega2560 pin 35
+//ICP5 - Arduino pin 48 - ATmega2560 pin 36
 
 void setup() {
   Serial.begin(9600, SERIAL_8O1);
-  delay(500);
 
   /* Set all pins on PORTL to input mode.
       Arduino pins 42, 43, 48, and 49 all belong
@@ -18,56 +19,37 @@ void setup() {
   DDRL |= 0;
 
   /* Timer 1 setup */
-  TIFR1 = (1 << ICF1);  // Clear pending interupts and Overflow Flag Interrupt
-  TCCR1A = 0;           // Disable compare interrupts (necessary for arduino) and set WGM to normal mode
-  TCCR1B = (0 << ICNC1) | // Turn off noise filtering
-           (1 << ICES1) |        // Set input capture edge to rising
-           (0 << WGM13) |        // Set WGM to normal
-           (0 << WGM12) |
-           (1 << CS11) |         // CS bits set prescale to /64
+  TIFR1 = (1 << ICF1);    // Clear pending interupts and Overflow Flag Interrupt
+  TCCR1A = 0;             // Disable compare interrupts (necessary for arduino) and set WGM to normal mode
+  TCCR1B = (0 << ICNC1)|  // Turn off noise filtering
+           (1 << ICES1)|  // Set input capture edge to rising
+           (0 << WGM13)|  // Set WGM to normal
+           (0 << WGM12)|
+           (1 << CS11)|   // CS bits set prescale to /64
            (1 << CS10);
-  TIMSK1 = (1 << ICIE1) | // Enable 16-bit Timer Capture Event Interrupt
-           (0 << TOIE1);         // Disable Overflow Interrupt
-  TIFR1 = (1 << TOV1);  // Clear timer overflow flag
+  TIMSK1 = (1 << ICIE1)|  // Enable 16-bit Timer Capture Event Interrupt
+           (0 << TOIE1);  // Disable Overflow Interrupt
 
   /* Timer 3 setup */
   TIFR3 = (1 << ICF3);
   TCCR3A = 0;
-  TCCR3B = (0 << ICNC3) |
-           (1 << ICES3) |
-           (0 << WGM33) |
-           (0 << WGM32) |
-           (1 << CS31) |
-           (1 << CS30);
-  TIMSK1 = (1 << ICIE3) |
-           (0 << TOIE3);
-  TIFR3 = (1 << TOV3);
+  TCCR3B = (0 << ICNC3)|(1 << ICES3)|(0 << WGM33)|
+           (0 << WGM32)|(1 << CS31)|(1 << CS30);
+  TIMSK3 = (1 << ICIE3)|(0 << TOIE3);
 
   /* Timer 4 setup */
   TIFR4 = (1 << ICF4);
   TCCR4A = 0;
-  TCCR4B = (0 << ICNC4) |
-           (1 << ICES4) |
-           (0 << WGM43) |
-           (0 << WGM42) |
-           (1 << CS41) |
-           (1 << CS40);
-  TIMSK4 = (1 << ICIE4) |
-           (0 << TOIE4);
-  TIFR4 = (1 << TOV4);
+  TCCR4B = (0 << ICNC4)|(1 << ICES4)|(0 << WGM43)|
+           (0 << WGM42)|(1 << CS41)|(1 << CS40);
+  TIMSK4 = (1 << ICIE4)|(0 << TOIE4);
 
   /* Timer 5 setup */
   TIFR5 = (1 << ICF5);
   TCCR5A = 0;
-  TCCR5B = (0 << ICNC5) |
-           (1 << ICES5) |
-           (0 << WGM53) |
-           (0 << WGM52) |
-           (1 << CS51) |
-           (1 << CS50);
-  TIMSK5 = (1 << ICIE5) |
-           (0 << TOIE5);
-  TIFR5 = (1 << TOV5);
+  TCCR5B = (0 << ICNC5)|(1 << ICES5)|(0 << WGM53)|
+           (0 << WGM52)|(1 << CS51)|(1 << CS50);
+  TIMSK5 = (1 << ICIE5)|(0 << TOIE5);
 
   /* Enable global interrupts */
   sei();
@@ -173,7 +155,7 @@ void loop() {
         TIFR4 = (1 << ICF4);
         TIFR5 = (1 << ICF5);
       }
-      delay(520);
+      _delay_ms(520);
     }
   }
 }
